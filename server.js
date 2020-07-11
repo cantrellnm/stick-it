8ac0da7e6225fb83b99da065875b3ee41e262f06
cantrellnm <cantrellnm@gmail.com>
2020-07-10 21:49:29 -0700

    Update mongodb and socket.io

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID;
-
-var url = process.env.DB_URL;
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
 app.use(express.static('public'));
 
@@ -14,37 +12,34 @@ app.get('/', function(request, response){
   response.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-io.on('connection', function(socket){
+const url = process.env.DB_URL;
+MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client){
+    const db = client.db();
 
-  MongoClient.connect(url, function(err, db){
-    if (err) console.log(err);
-    db.collection('posts').find({note: {$exists: true}}).each(function(err, doc){
-      if (err) console.log(err);
-      if (doc !== null) socket.emit('new post', doc);
-    });
-  });
-  
-  socket.on('new', function(post){
-    MongoClient.connect(url, function(err, db){
-      db.collection('posts').insert(post, function(err, result){
+    io.on('connection', function(socket){
+      db.collection('posts').find({note: {$exists: true}}).each(function(err, doc){
         if (err) console.log(err);
-        io.emit('new post', result.ops[0]);
-      });;
+        if (doc !== null) socket.emit('new post', doc);
+      });
+
+      socket.on('new', function(post){
+        db.collection('posts').insertOne(post, function(err, result){
+          if (err) console.log(err);
+          io.emit('new post', result.ops[0]);
+        });
+      });
+
+      socket.on('remove', function(post){
+        db.collection('posts').removeOne({'_id': new ObjectID(post._id)}, function(err, result){
+          if (err) console.log(err);
+          if (result.n === 0) {
+            console.log("no such post");
+          } else {
+            io.emit('remove post', post._id);
+          }
+        });
+      });
     });
-  });
-  
-  socket.on('remove', function(post){
-    MongoClient.connect(url, function(err, db){      
-      db.collection('posts').remove({'_id': new ObjectID(post._id)}, function(err, result){
-        if (err) console.log(err);
-        if (result.n === 0) {
-          console.log("no such post");  
-        } else {
-          io.emit('remove post', post._id); 
-        }
-      });;
-    });
-  });
+
+    http.listen(process.env.PORT || 3000);
 });
-
-http.listen(process.env.PORT || 3000);
